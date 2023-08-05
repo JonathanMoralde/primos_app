5786b3212d054e536f02993d3209970ba2f37f0f
Jack <jackvincentsanchez.pobre@gmail.com>
2023-08-05 12:28:22 +0800

    Included firebase authentication and configured files with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'package:primos_app/pages/loginScreen.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(
     const ProviderScope(
       child: MyApp(),
