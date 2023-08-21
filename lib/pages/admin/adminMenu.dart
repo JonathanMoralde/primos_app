fb2156fe3b0117924f44512a0e65551d3c43d503
Jonathan Moralde <jonathanhernandez.moralde@bicol-u.edu.ph>
2023-08-22 00:56:17 +0800

    working filterBtns in adminMenu

--- lib/pages/admin/adminMenu.dart
+++ lib/pages/admin/adminMenu.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:primos_app/pages/admin/adminMenu_CatForm.dart';
 import 'package:primos_app/pages/admin/adminMenu_Form.dart';
+import 'package:primos_app/providers/categoryFilter/activeCategory_provider.dart';
 import 'package:primos_app/widgets/bottomBar.dart';
 import 'package:primos_app/widgets/filterBtns.dart';
 import 'package:primos_app/widgets/itemCard.dart';
@@ -10,21 +12,19 @@ import 'package:primos_app/widgets/searchBar.dart';
 import 'package:primos_app/widgets/styledButton.dart';
 import 'package:primos_app/widgets/pageObject.dart';
 
+import '../../providers/waiter_menu/menuItems_provider.dart';
+
 // TODO REFACTOR THIS PAGE TO USE RIVERPOD STATE MANAGEMENT
 
-class AdminMenuPage extends StatefulWidget {
-  const AdminMenuPage({Key? key});
+class AdminMenuPage extends ConsumerWidget {
+  AdminMenuPage({Key? key});
+
+  // final CollectionReference itemsCollection =
+  //     FirebaseFirestore.instance.collection('menu');
 
   @override
-  _AdminMenuPageState createState() => _AdminMenuPageState();
-}
-
-class _AdminMenuPageState extends State<AdminMenuPage> {
-  final CollectionReference itemsCollection =
-      FirebaseFirestore.instance.collection('menu');
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final menuItems = ref.watch(menuItemsStreamProvider);
     return Scaffold(
       backgroundColor: const Color(0xFFF8F8F7),
       drawer: SideMenu(pages: adminPages),
@@ -39,47 +39,87 @@ class _AdminMenuPageState extends State<AdminMenuPage> {
               children: [
                 CustomSearchBar(),
                 FilterBtns(),
-                StreamBuilder<QuerySnapshot>(
-                  stream: itemsCollection.snapshots(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasError) {
-                      return Text('Error: ${snapshot.error}');
-                    }
+                Consumer(builder: ((context, ref, child) {
+                  final activeCategory = ref.watch(activeCategoryProvider);
+                  return menuItems.when(
+                      data: (itemDocs) {
+                        // FILTER BASED ON ACTIVE CATEGORY
+                        final filteredItems = itemDocs.where((itemDoc) {
+                          final productCategory = itemDoc['category'] as String;
+                          return activeCategory == "All" ||
+                              productCategory == activeCategory;
+                        }).toList();
 
-                    if (!snapshot.hasData) {
-                      return CircularProgressIndicator();
-                    }
+                        return Padding(
+                          padding: EdgeInsets.all(16),
+                          child: Wrap(
+                            spacing: 10,
+                            runSpacing: 10,
+                            children: filteredItems.map((itemDoc) {
+                              final productId = itemDoc.id;
+                              final productName = itemDoc['itemName'] as String;
+                              final itemPrice = itemDoc['itemPrice'];
+                              final double productPrice = (itemPrice is double)
+                                  ? itemPrice
+                                  : (itemPrice is int)
+                                      ? itemPrice.toDouble()
+                                      : 0.0;
+                              final imageUrl = itemDoc['imageURL'] as String;
 
-                    final itemDocs = snapshot.data!.docs;
+                              return ItemCard(
+                                productId: productId, // Pass the productId
+                                productName: productName,
+                                productPrice: productPrice,
+                                imageUrl: imageUrl,
+                              );
+                            }).toList(),
+                          ),
+                        );
+                      },
+                      error: (error, stackTrace) => Text("Error: $error"),
+                      loading: () => CircularProgressIndicator());
+                }))
+                // StreamBuilder<QuerySnapshot>(
+                //   stream: itemsCollection.snapshots(),
+                //   builder: (context, snapshot) {
+                //     if (snapshot.hasError) {
+                //       return Text('Error: ${snapshot.error}');
+                //     }
 
-                    return Padding(
-                      padding: EdgeInsets.all(16),
-                      child: Wrap(
-                        spacing: 10,
-                        runSpacing: 10,
-                        children: itemDocs.map((itemDoc) {
-                          final productId = itemDoc
-                              .id; // Get the document ID as the productId
-                          final productName = itemDoc['itemName'] as String;
-                          final itemPrice = itemDoc['itemPrice'];
-                          final double productPrice = (itemPrice is double)
-                              ? itemPrice
-                              : (itemPrice is int)
-                                  ? itemPrice.toDouble()
-                                  : 0.0;
-                          final imageUrl = itemDoc['imageURL'] as String;
+                //     if (!snapshot.hasData) {
+                //       return CircularProgressIndicator();
+                //     }
 
-                          return ItemCard(
-                            productId: productId, // Pass the productId
-                            productName: productName,
-                            productPrice: productPrice,
-                            imageUrl: imageUrl,
-                          );
-                        }).toList(),
-                      ),
-                    );
-                  },
-                ),
+                //     final itemDocs = snapshot.data!.docs;
+
+                //     return Padding(
+                //       padding: EdgeInsets.all(16),
+                //       child: Wrap(
+                //         spacing: 10,
+                //         runSpacing: 10,
+                //         children: itemDocs.map((itemDoc) {
+                //           final productId = itemDoc
+                //               .id; // Get the document ID as the productId
+                //           final productName = itemDoc['itemName'] as String;
+                //           final itemPrice = itemDoc['itemPrice'];
+                //           final double productPrice = (itemPrice is double)
+                //               ? itemPrice
+                //               : (itemPrice is int)
+                //                   ? itemPrice.toDouble()
+                //                   : 0.0;
+                //           final imageUrl = itemDoc['imageURL'] as String;
+
+                //           return ItemCard(
+                //             productId: productId, // Pass the productId
+                //             productName: productName,
+                //             productPrice: productPrice,
+                //             imageUrl: imageUrl,
+                //           );
+                //         }).toList(),
+                //       ),
+                //     );
+                //   },
+                // ),
               ],
             ),
           ),
